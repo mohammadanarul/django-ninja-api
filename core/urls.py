87459e8f222e9ceea329b26e60528bc1bf47abe1
mohammadanarul <mdanarul7075@gmail.com>
2023-02-08 01:57:12 +0600

    core urls path chagne. django ninja is very awesome working with django.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog.views import api
+from blog.api.api import blogApi
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/v1/', api.urls)
+    path('api/v1/blog/', blogApi.urls)
 ]
